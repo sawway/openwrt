aa9e1975df18b184d301c32620d881fe7e63c6e7
sawway <61615591+sawway@users.noreply.github.com>
2021-08-18 20:15:01 +0800

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+ws=$(pwd)
+
+src=$ws/src
+openwrt=openwrt
+
+mkdir -p $src
+cd $src
+
+git config --global http.lowSpeedLimit 0
+git config --global http.lowSpeedTime 999999
+git config --global http.postBuffer 524288000
+
+git config --global  ssh.lowSpeedLimit 0
+git config --global  ssh.lowSpeedTime 999999
+git config --global  ssh.postBuffer 524288000
+
+git clone git://git.openwrt.org/openwrt/openwrt.git -b openwrt-18.06  $openwrt
+
+cd $openwrt
+
+
+du -lh --max-depth=1
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+touch ./.config
+echo "CONFIG_TARGET_IMAGES_GZIP=y" >> ./.config
+
+make defconfig
+
+make download -j8 || make download -j1 V=s
+
+
+make -j$(nproc) V=s|| make -j1 V=s
+
+du -lh --max-depth=1
+
